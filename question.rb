4503481c0ecc8533bf338a79905b2baa633c42b1
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-22 20:47:15 +0000

    question autogenerates new addition problems each loop

--- question.rb
+++ question.rb
@@ -1,18 +1,19 @@
 class Question
 
   def initialize
-    @answer = 4
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
     @user_guess = nil
   end
 
   def ask_question
-    puts "What is 2 + 2?"
+    puts "What is #{@num1} + #{@num2}?"
   end
 
   def answer_question
     @user_guess = gets.chomp.to_i
 
-    if @user_guess == @answer
+    if @user_guess == @num1 + @num2
       puts "Great job!"
       true
     else
